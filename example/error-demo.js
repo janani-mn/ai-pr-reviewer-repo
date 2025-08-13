76c4a53de23276ad423522cb670e7976600ed135
janani-mn <JANANIMN14@GMAIL.COM>
2025-08-13 11:17:42 +0530

    Update error-demo.js

--- example/error-demo.js
+++ example/error-demo.js
@@ -1,20 +1,7 @@
-import React from 'react';
-
-// This component intentionally contains several common React errors for testing PR review
 function ErrorDemoComponent(props) {
-  // Error: Unused variable
-  const unused = 123;
-
-  // Error: Typo in props (should be props.title)
-  const heading = props.titel;
-
-  // Error: JSX not wrapped in a single parent element
-  <h1>{heading}</h1>
-  <p>This is a demo component with errors.</p>
-
-  // Error: Missing return statement
+  return (
+    <div>
+      <h1>{props.title}
+    </div>
+  );
 }
-
-// Error: Missing propTypes validation
-
-export default ErrorDemoComponent;
